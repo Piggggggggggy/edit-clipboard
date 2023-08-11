93c0faead9d0e94d1ae1c6e89418611db98d3ee2
Ethan Shiota <piggy.god3@gmail.com>
2023-08-10 21:12:05 -0700

    rename

--- src/Preprocesser/mod.rs
+++ src/Preprocesser/mod.rs
@@ -1,19 +1,22 @@
 pub trait Preprocesser {
-    pub fn process(self, text: &mut String) {}
+    fn process(&self, text: &mut String) {}
 }
 
 pub struct Processor(Vec<Box<dyn Preprocesser>>);
 // define module basetrait
 impl Processor {
+    pub fn new() -> Self {
+        Processor(Vec::new())
+    }
     pub fn apply(self, text: &mut String) {
         for item in self.0 {
-            *item.process(text);
+            item.process(text);
         }
     }
 
     // Adds operation to processor stack
-    pub fn add_op(&mut self, op: dyn Preprocesser) {
-        self.0.push(Box::new(op));
+    pub fn add_op(&mut self, op: Box<dyn Preprocesser>) {
+        self.0.push(op);
     }
     // todo
     // fn order()
