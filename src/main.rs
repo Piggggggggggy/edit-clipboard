da9a2e5ae1fd0e0d231204d1850c124ad4c57022
Ethan Shiota <piggy.god3@gmail.com>
2023-08-10 21:11:14 -0700

    refactored main and improved processor

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,38 @@
+mod preprocesser;
+
+use crate::preprocesser::Preprocesser;
 use clipboard::ClipboardContext;
 use clipboard::ClipboardProvider;
 use inquire::Confirm;
+use preprocesser::Processor;
 use std::process::Stdio;
 
-const EDITOR_COMMAND: &'static str = "hx";
+// todo make this read in from a config file
+const EDITOR_COMMAND: &str = "hx";
+
 fn main() {
     let mut ctx: ClipboardContext = ClipboardProvider::new().expect("could not get provider");
     // Create a temp file with clipboard contents prompting if it is non-text or undefined.
-    let tempfile = temp_file::with_contents({
-        if let Ok(str) = ctx.get_contents() {
-            str
-        } else if Confirm::new("clipboard is undefined or non-text, do you want to writeover it?")
-            .prompt()
-            .expect("prompting failed")
-        {
+    let confirm_overwrite =
+        Confirm::new("clipboard is undefined or non-text, do you want to overwrite it?");
+    let mut text = {
+        if let Ok(clipboard_contents) = ctx.get_contents() {
+            clipboard_contents
+        } else if confirm_overwrite.prompt().unwrap() {
             String::from("")
         } else {
             std::process::exit(0)
         }
-        .as_bytes()
-    });
+    };
+
+    // apply preprocesser(s)
+    let processor = Processor::new();
+    processor.apply(&mut text);
+
+    // Create tempfile for editor
+    let tempfile = temp_file::with_contents(text.as_bytes());
     // Creates a helix process to edit the file
-    let mut helix = std::process::Command::new(EDITOR_COMMAND)
+    let mut editor_process = std::process::Command::new(EDITOR_COMMAND)
         .stdin(Stdio::inherit())
         .stdout(Stdio::inherit())
         .env_clear()
@@ -29,7 +40,7 @@ fn main() {
         .spawn()
         .unwrap();
     // Wait for helix to exit -> i.e. editing is done
-    helix.wait().expect("helix should not fail");
+    editor_process.wait().expect("helix should not fail");
     // Sets clipboard contents to file
     ctx.set_contents(std::fs::read_to_string(tempfile.path()).unwrap())
         .unwrap();
